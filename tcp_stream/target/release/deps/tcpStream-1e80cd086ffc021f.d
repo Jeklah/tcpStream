1913ba0babc5a7dac6b28d2ee4cb2bc3ccdab4b5
Jeklah <arthurbowers@gmail.com>
2022-08-29 16:01:35 +0100

    Adding rust release compiled executable.

---
+++ tcp_stream/target/release/deps/tcpStream-1e80cd086ffc021f.d
@@ -0,0 +1,5 @@
+/home/kali/projects/tcpStream/tcp_stream/target/release/deps/tcpStream-1e80cd086ffc021f: src/main.rs
+
+/home/kali/projects/tcpStream/tcp_stream/target/release/deps/tcpStream-1e80cd086ffc021f.d: src/main.rs
+
+src/main.rs:
---
+++ tcp_stream/target/release/tcpStream.d
@@ -0,0 +1,1 @@
+/home/kali/projects/tcpStream/tcp_stream/target/release/tcpStream: /home/kali/projects/tcpStream/tcp_stream/src/main.rs
